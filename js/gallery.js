a6bf542070e973af83b8d594b75da802befed6ea
Dayla <64294351+dayla-cloud@users.noreply.github.com>
2022-01-23 18:07:01 +0900

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 const imgBoard = document.querySelector("#gallery-img");
 
-const imgArray = ["1", "2", "3", "4", "5", "6"];
+const imgArray = ["1", "3", "4", "5", "6"];
 const imgRandom = imgArray[Math.floor(Math.random() * imgArray.length)];
 
 imgBoard.src = `./asset/gallery/${imgRandom}.jpg`;
